5d4b4eb078b52d216475a7a0e5507f34f9299ee0
david-kavenga <david.kavenga@gmail.com>
2022-07-25 11:38:40 +1200

    initial commit

---
+++ src/main/java/com/dining/DiningAPI/DiningApiApplication.java
@@ -0,0 +1,13 @@
+package com.dining.DiningAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningApiApplication.class, args);
+	}
+
+}
