8f9c98910510b42fbe759627f5b7a7587712f963
david-kavenga <david.kavenga@gmail.com>
2022-07-25 15:45:05 +1200

    Add User entity

---
+++ src/main/java/com/dining/diningAPI/model/User.java
@@ -0,0 +1,40 @@
+package com.dining.diningAPI.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.NonNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @NonNull
+  @Column(unique=true)
+  private String name;
+
+  @NonNull
+  private String city;
+
+  @NonNull
+  private String state;
+
+  @NonNull
+  private String zipcode;
+
+  private Boolean peanutAllergyInterest;
+  private Boolean eggAllergyInterest;
+  private Boolean dairyAllergyInterest;
+}
