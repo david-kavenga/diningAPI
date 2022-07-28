a5afdd05b8d787622577da3e811e982c5c5d8c07
david-kavenga <david.kavenga@gmail.com>
2022-07-28 14:59:36 +1200

    Add basic restaurant seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (displayName, city, state, zipCode, peanutAllergyInterest, eggAllergyInterest, dairyAllergyInterest) VALUES ('David', 'Tauranga', 'Bay of Plenty', '111', FALSE, FALSE, FALSE);
 INSERT INTO users (displayName, city, state, zipCode, peanutAllergyInterest, eggAllergyInterest, dairyAllergyInterest) VALUES ('Ron', 'Seoul', 'Seoul', '222', FALSE, TRUE, FALSE);
+INSERT INTO restaurant (name, city, state, zipCode) VALUES ('David Restaurant', 'Tauranga', 'Bay of Plenty', '111');
+INSERT INTO restaurant (name, city, state, zipCode) VALUES ('Ron Restaurant', 'Seoul', 'Seoul', '222');
