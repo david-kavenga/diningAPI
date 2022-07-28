427a32e85f4a899429718fa3d3ff2ec759ab1333
david-kavenga <david.kavenga@gmail.com>
2022-07-28 14:36:07 +1200

    Add user put endpoint with minimal validation

--- src/main/java/com/dining/diningAPI/controller/DiningReviewController.java
+++ src/main/java/com/dining/diningAPI/controller/DiningReviewController.java
@@ -2,9 +2,14 @@ package com.dining.diningAPI.controller;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.dining.diningAPI.exceptions.QueryNotSupportedException;
 import com.dining.diningAPI.model.*;
@@ -33,9 +38,46 @@ public class DiningReviewController {
   public User getUser(@PathVariable String displayName) throws QueryNotSupportedException{
     Optional<User> userOptional = userRepository.findByDisplayName(displayName);
     if(userOptional.isEmpty()){
-      throw new QueryNotSupportedException("Error: Query not supported. User of specified display name not found.");
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "DisplayName specified does not match a user");
     }
     return userOptional.get();
+  }
+
+  @PostMapping("/users")
+  public User addUser(@RequestBody User newUser){
+    return userRepository.save(newUser);
+  }
+
+  @PutMapping("/users/{displayName}")
+  public User updateUser(@RequestBody User newUser, @PathVariable String displayName){
+    Optional<User> userOptional = userRepository.findByDisplayName(displayName);
+    if(!userOptional.isPresent()){
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "DisplayName specified does not match a user");
+    }
+
+    User userToUpdate = userOptional.get();
+
+    if(newUser.getCity() != null){
+      userToUpdate.setCity(newUser.getCity());
+    }
+    if(newUser.getState() != null){
+      userToUpdate.setState(newUser.getState());
+    }
+    if(newUser.getZipcode() != null){
+      userToUpdate.setZipcode(newUser.getZipcode());
+    }
+    if(newUser.getPeanutAllergyInterest() != null){
+      userToUpdate.setPeanutAllergyInterest(newUser.getPeanutAllergyInterest());
+    }
+    if(newUser.getEggAllergyInterest() != null){
+      userToUpdate.setEggAllergyInterest(newUser.getEggAllergyInterest());
+    }
+    if(newUser.getDairyAllergyInterest() != null){
+      userToUpdate.setDairyAllergyInterest(newUser.getDairyAllergyInterest());
+    }
+
+    userRepository.save(userToUpdate);
+    return userToUpdate;
   }
 
   public Boolean isValidUser(String displayName){
