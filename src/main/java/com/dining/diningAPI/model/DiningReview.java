1ec739f1b35ee8488035bceab62485b63b2dde2f
david-kavenga <david.kavenga@gmail.com>
2022-07-30 16:03:35 +1200

    Add DiningReview seed values

--- src/main/java/com/dining/diningAPI/model/DiningReview.java
+++ src/main/java/com/dining/diningAPI/model/DiningReview.java
@@ -1,6 +1,9 @@
 package com.dining.diningAPI.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -32,6 +35,8 @@ public class DiningReview {
   @NonNull
   private Long restaurantId;
 
+  @Column(length = 32, columnDefinition = "varchar(32) default 'PENDING'")
+  @Enumerated(value = EnumType.STRING)
   private Status reviewStatus = Status.PENDING;
   private Integer peanutScore;
   private Integer eggScore;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO users (displayName, city, state, zipCode, peanutAllergyInterest, egg
 INSERT INTO users (displayName, city, state, zipCode, peanutAllergyInterest, eggAllergyInterest, dairyAllergyInterest) VALUES ('Ron', 'Seoul', 'Seoul', '222', FALSE, TRUE, FALSE);
 INSERT INTO restaurant (name, city, state, zipCode) VALUES ('David Restaurant', 'Tauranga', 'Bay of Plenty', '111');
 INSERT INTO restaurant (name, city, state, zipCode) VALUES ('Ron Restaurant', 'Seoul', 'Seoul', '222');
+INSERT INTO diningreview (displayName, zipCode, restaurantId, peanutScore) VALUES ('David', '111', 1, 4);
+INSERT INTO diningreview (displayName, zipCode, restaurantId, eggScore) VALUES ('David', '222', 1, 4);
